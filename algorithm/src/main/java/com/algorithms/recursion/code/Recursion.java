93aa7498bbca2d07daa7e9ec181d62b8279265a8
Aakash <aakash@Aakashs-MacBook-Pro.local>
2022-07-23 20:18:58 +0530

    file update

--- algorithm/src/main/java/com/algorithms/recursion/code/Recursion.java
+++ algorithm/src/main/java/com/algorithms/recursion/code/Recursion.java
@@ -1,2 +1,13 @@
-package com.algorithms.recursion.code;public class Recursion {
+package com.algorithms.recursion.code;
+
+public class Recursion {
+    public static int factorial(int num){
+        return num==0?1:num*factorial(num-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+        System.out.println(factorial(10));
+        System.out.println(factorial(9));
+    }
 }
