0192a61dcdfcb964f62d787652a853cd2820a4fd
Aakash <aakash@Aakashs-MacBook-Pro.local>
2022-07-23 20:37:31 +0530

    updated base condition for recursion

--- algorithm/src/main/java/com/algorithms/recursion/code/Recursion.java
+++ algorithm/src/main/java/com/algorithms/recursion/code/Recursion.java
@@ -2,12 +2,20 @@ package com.algorithms.recursion.code;
 
 public class Recursion {
     public static int factorial(int num){
-        return num==0?1:num*factorial(num-1);
+        if (num < 0){
+            return -1;
+        }
+        if (num==1 || num==0){
+            return 1;
+        }
+        return num*factorial(num-1);
     }
 
     public static void main(String[] args) {
         System.out.println(factorial(4));
         System.out.println(factorial(10));
-        System.out.println(factorial(9));
+        System.out.println(factorial(0));
+        System.out.println(factorial(1));
+        System.out.println(factorial(-1));
     }
 }
